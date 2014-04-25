cebbbb1e215b08f305ac17832022aa10536819f5
Pyramidi <lonely@tfwno.gf>
2014-04-26 00:07:14 +0300

    Added yle.py and updated ask.py

--- ask.py
+++ ask.py
@@ -3,25 +3,44 @@ ask.py - Willie Ask Module
 Original author: Meicceli
 Licensed under the GNU Lesser General Public License Version 3 (or greater at your wish).
 """
-
 from willie.module import commands
-from random import choice
+import random
 
 
-@commands('ask')
-def rand(bot, trigger):
-    answers = ['yes.', 'no.', 'maybe.']
+@commands('homopaska')
+def randeoaie(bot, trigger):
+    answers = ['y', 'n']
 
-    #Saves the message without the ".ask " part into a variable
-    message = trigger.group().replace('.ask ', '')
+    # Stores the message into the variable
+    message = trigger.group(2)
 
-    #Checks if there's the word "or" in the message
-    if message.find(' or ') != -1:
-        #Cuts the user given choices into a list
-        user_choices = message.split(' or ')
-        bot.reply(choice(user_choices) + " of course!")
+    # Checks if the message has both "or" and "&&" in it
+    if message.find(' or ') != -1 and message.find(' && ') != -1:
+        answer = ""
+        # Splits the questions and iterates through them
+        for i in message.split(' && '):
+            # Answers the "or" questions and appends the answer into
+            if i.find(' or ') != -1:
+                choices = i.split(' or ')
+                answer += random.choice(choices) + " of course! and "
+            # Answers the questions without "or"
+            else:
+                answer += random.choice(answers) + " and "
+        bot.reply(answer[:-5])
+        return
 
-    #If there is no word "or" in the user's message,
-    #then just reply with a pseudorandom answer
+    # Checks if there is only "or"-questions in the message
+    elif message.find(' or ') != -1:
+        choices = message.split(' or ')
+        bot.reply(random.choice(choices) + " of course!")
+
+    # Checks if there are multiple questions without "or"
+    elif message.find(' && ') != -1:
+        choices = message.split(' && ')
+        output = ""
+        for i in choices:
+            output += random.choice(answers) + " and "
+        bot.reply(output[:-5])
+
     else:
-        bot.reply(choice(answers))
+        bot.reply(random.choice(answers))
