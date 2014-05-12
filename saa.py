283612f99f9d42d30514e814eff45d5540041548
Pyramidi <lonely@tfwno.gf>
2014-05-13 00:00:59 +0300

    Updated saa.py

--- saa.py
+++ saa.py
@@ -1,7 +1,7 @@
 # coding=utf8
 """
 weather.py - Willie Open Weather Map Module
-Copyright 2014, Marcus Leivo
+Original author: Meicceli
 Licensed under the GNU Lesser General Public License Version 3 (or greater at your wish).
 
 http://willie.dftba.net
@@ -13,6 +13,8 @@ from willie.module import commands, example
 
 import json
 import datetime
+import time
+import os
 
 
 def setup(bot):
@@ -21,13 +23,14 @@ def setup(bot):
         bot.db.preferences.add_columns(['location'])
 
 
+# Gets the weather
 def owm_get(location):
-    apiurl = "http://api.openweathermap.org/data/2.5/weather?q=" + location
+    apiurl = "http://api.openweathermap.org/data/2.5/weather?units=metric&q=" + location
     resp = json.loads(web.get(apiurl))
     if resp['cod'] == "404":
         return "Error: City not found."
 
-    # Variables
+    # VARIABLES
     output = ""
     observation_time = ""
     sunrise = ""
@@ -38,66 +41,96 @@ def owm_get(location):
     cloudness = ""
     gust = ""
     wind_speed = ""
-    wind_direction = ""
 
-    # Times
-    if 'dt' in resp:
-        # UNIX time -> Readable time (server's timezone)
-        observation_time = str(datetime.datetime.fromtimestamp(int(resp['dt'])).strftime('%H:%M:%S'))
+    # TIMES
+    # If OWM finds coordinates for the location, tries getting localized sunrise and -set
+    if "coord" in resp:
+        timeurl = "http://api.geonames.org/timezoneJSON?username=meicceli&lat=" + str(resp["coord"]["lat"]) + "&lng=" + str(resp["coord"]["lon"])
+        suntime = json.loads(web.get(timeurl))
 
-    if 'sunrise' in resp['sys']:
-        sunrise = "Sunrise: " + \
-            str(datetime.datetime.fromtimestamp(int(resp['sys']['sunrise'])).strftime('%H:%M:%S')) + "; "
+        if 'dt' in resp:
+            # Localize the time
+            os.environ['TZ'] = suntime['timezoneId']
+            time.tzset()
+            time.tzname
+            # UNIX time -> Readable time (server's timezone)
+            observation_time = str(datetime.datetime.fromtimestamp(int(resp['dt'])).strftime('%H:%M:%S'))
 
-    if 'sunset' in resp['sys']:
-        sunset = "Sunset: " + \
-            str(datetime.datetime.fromtimestamp(int(resp['sys']['sunset'])).strftime('%H:%M:%S'))
+        # Checks if sunrise and suntime are available and if so, gets them
+        if "sunrise" in suntime:
+            sunrise = "Sunrise: " + suntime["sunrise"].split(" ")[1] + "; "
+        if "sunset" in suntime:
+            sunset = "Sunset: " + suntime["sunset"].split(" ")[1] + "; "
 
+    # If OWM cant get the coordinates, tries getting sunrise and sunset from OWM itself. The UNIX time from the api will then not be converted into
+    # the location's timezone. Instead it converts the UNIX time to your the server's time Willie is running on.
+    else:
+        if 'sunrise' in resp['sys']:
+            sunrise = "Sunrise: " + str(datetime.datetime.fromtimestamp(int(resp['sys']['sunrise'])).strftime('%H:%M:%S')) + "; "
 
-    # Weather
+        if 'sunset' in resp['sys']:
+            sunset = "Sunset: " + str(datetime.datetime.fromtimestamp(int(resp['sys']['sunset'])).strftime('%H:%M:%S'))
+
+    # WEATHER
     if 'humidity' in resp['main']:
         humidity = "Humidity: " + str(resp['main']['humidity']) + "%" + "; "
 
     if 'temp' in resp['main']:
-        temperature = "Temperature: " + str(round(resp['main']['temp'] - 273.15, 1)) + "°C" + "; "
+        # Must convert kelvins into Celsius and Fahrenheit
+        temp_celsius = str(resp['main']['temp'])
+        temperature = "Temperature: " + temp_celsius + "°C; "
 
     if 'pressure' in resp['main']:
         pressure = "Air pressure: " + \
             str(resp['main']['pressure']) + "hPa" + "; "
 
     if 'description' in resp['weather'][0]:
-        cloudness = "Sky: " + resp['weather'][0]['description'] + "; "
+        cloudness = resp['weather'][0]['description'] + "; "
 
-
-    # Wind
+    # WIND
     if 'gust' in resp['wind']:
         gust = "Gust: " + str(resp['wind']['gust']) + "m/s" + "; "
 
-    if 'speed' in resp['wind']:
-        wind_speed = "Wind speed: " + \
-            str(resp['wind']['speed']) + "m/s" + "; "
+    if 'speed' in resp['wind'] and 'deg' in resp['wind']:
+        wind_speed = "Wind: " + str(resp['wind']['speed']) + "m/s (" + wind_dir(resp['wind']['deg']) + "); "
 
-    if 'deg' in resp['wind']:
-        wind_direction = "Wind direction: " + \
-            str(int(round(resp['wind']['deg'], -1))) + "°" + "; "
-
-
-    if 'name' in resp:
-        output = resp['name'] + " " + observation_time + " - "
+    # Generates the output
+    if 'name' in resp and 'country' in resp['sys']:
+        output = resp['name'] + ", " + resp['sys']['country'] + " " + observation_time + " - "
     output += temperature
-    output += humidity
     output += wind_speed
     output += gust
-    output += wind_direction
+    output += humidity
     output += pressure
     output += cloudness
     output += sunrise
     output += sunset
-    output += " (source: openweathermap.org)"
+    output += "(openweathermap.org)"
 
     return output
 
 
+# Converts wind direction (degrees) into a readable format
+def wind_dir(degrees):
+    if (degrees >= 354.38 or degrees < 39.38):
+        return 'North'
+    elif (degrees >= 39.38 and degrees < 84.38):
+        return 'Northeast'
+    elif (degrees >= 84.38 and degrees < 129.38):
+        return 'East'
+    elif (degrees >= 129.38 and degrees < 174.38):
+        return 'Southeast'
+    elif (degrees >= 174.38 and degrees < 219.38):
+        return 'South'
+    elif (degrees >= 219.38 and degrees < 264.38):
+        return 'Southwest'
+    elif (degrees >= 264.38 and degrees < 309.38):
+        return 'West'
+    elif (degrees >= 309.38 and degrees < 354.38):
+        return 'Northwest'
+
+
+# Search for the given location
 def location_search(location_to_search):
     search_url = "http://api.openweathermap.org/data/2.5/find?mode=json&q=" + location_to_search
     search_sauce = json.loads(web.get(search_url))
@@ -112,8 +145,8 @@ def location_search(location_to_search):
         return False
 
 
-@commands('sää')
-@example('.sää Helsinki')
+@commands('sää', 'wea', 'weather')
+@example('.wea Helsinki')
 def saa_owm(bot, trigger):
     user_loc = trigger.group(2)
 
@@ -124,24 +157,26 @@ def saa_owm(bot, trigger):
             user_loc = bot.db.preferences.get(trigger.nick, 'location')
 
         if not user_loc:
-            bot.reply("I don't know where you live. " +
-                'Give me a location, like .weather London, or tell me where you live by saying .setlocation London, for example.')
+            bot.reply("I don't know where you live. Give me a location, like .weather London, or tell me where you live by saying .setlocation London, for example.")
             return
 
     # Find the proper location name to look up weather data from
-    location = location_search(user_loc)
-    if location != False:
+    location = user_loc
+    if location is not False:
         bot.say(owm_get(location))
     else:
         bot.reply("Location not found")
 
-@commands('setlocation', 'setwoeid')
+
+@commands('setlocation')
 @example('.setlocation Columbus, OH')
 def update_location(bot, trigger):
     """Set your default weather location."""
     user_location = location_search(trigger.group(2))
 
-    bot.db.preferences.update(trigger.nick, {'location': user_location})
-    if user_location is None:
+    if user_location is (None or False):
         bot.reply("Location not found.")
+        return
+
+    bot.db.preferences.update(trigger.nick, {'location': user_location})
     bot.reply('I now have you at ' + user_location)
