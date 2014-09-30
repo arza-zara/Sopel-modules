db1e37de1355902f1d2c14368a14b5a33ffa661e
Pyramidi <lonely@tfwno.gf>
2014-09-30 16:08:51 +0300

    [bmi.py] updated bmi.py

--- bmi.py
+++ bmi.py
@@ -1,72 +1,70 @@
+# -*- coding: utf-8 -*-
 """
-bmi.py - Willie Simple bmi-counter (SI-units) Module
+bmi.py - Willie BMI Module
 Original author: Meicceli
 Licensed under the GNU Lesser General Public License Version 3 (or greater at your wish).
 """
-from willie.module import commands, example
+from willie.module import commands, example, rate
 
 
+#@rate(60)
 @commands('bmi')
 @example('.bmi 60 1.90')
 def bmi(bot, trigger):
+    """.bmi <paino> <pituus>"""
     args = trigger.group()[5:].split()
-    #Makes sure that both weight and height are given
+    #Tarkastaa onko molemmat, paino ja pituus annettu
     if len(args) < 2:
-        bot.reply(".bmi <weight> <height>")
+        bot.reply("Voi vitun urpo :D Se on .bmi <paino> <pituus>")
         return
 
-    #Saves the first given argument as weight and the second argument as height
-    #Converts the decimal pointer into a dot if needed
-    weight = str(args[0]).replace(',', '.')
-    #Converts the decimal pointer into a dot if needed
-    height = str(args[1]).replace(',', '.')
+    #Asettaa painoksi ensiksi annetun luvun ja
+    #pituudeksi jalkimmaisen luvun
+    paino = str(args[0]).replace(',', '.')
+    pituus = str(args[1]).replace(',', '.')
 
-    #Makes sure that there are only digits (and an decimal pointer)
-    #in the given weight
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
-    for i in range(len(weight)):
-        if str(weight[i]) not in numbers:
-            bot.reply(".bmi <weight> <height>")
+    #Tarkastaa onko painossa kirjaimia ja
+    #jos on, keskeyttaa ohjelman
+    numerot = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+    for i in range(len(paino)):
+        if str(paino[i]) not in numerot:
+            bot.reply(".bmi <paino> <pituus>")
             return
 
-    #Makes sure that there are only digits (and an decimal pointer)
-    #in the given height
-    for i in range(len(height)):
-        if str(height[i]) not in numbers:
-            bot.reply(".bmi <weight> <height>")
+    #Tarkastaa onko pituudessa kirjaimia
+    for i in range(len(pituus)):
+        if str(pituus[i]) not in numerot:
+            bot.reply(".bmi <paino> <pituus>")
             return
 
-    #Calculates the bmi and responds accordingly
+    #Laskee painoindeksin ja vastaa asianmukaisesti
     else:
-        #If the given height is over 3 (metres), the user has presumably
-        #given the height in centimetres rather than in metres. This
-        #converts the height from centimetres into metres.
-        if float(height) > 3:
-            height = float(float(height) / 100)
+        #Jos annettu pituus on yli 3, oletettavasti on syotetty
+        #pituus senttimetreina. Tama muuntaa pituuden metreiksi
+        if float(pituus) > 3:
+            pituus = float(float(pituus) / 100)
 
-        #Calculates the body mass index
-        bodymassindex = float(weight) / float(height) ** 2
+        #Laskee painoindeksin
+        fatness = float(paino) / float(pituus) ** 2
+        #Laskee kuinka paljon tulee laihduttaa astraalipainoon
+        laihdutettava = round(float(paino) - float(pituus) ** 2 * 17.0, 2)
+        kasvettava = round(((float(paino) / 17.0) ** 0.5 - float(pituus)) * 100.0, 2)
 
-        #Calculates how much the user has to lose weight
-        #in order to be considered as normal weight
-        to_lose = float(weight) - float(height) ** 2 * 17.0
-        to_gain = -1 * to_lose
-
-        #Compares the user's bmi into this chart
-        #https://en.wikipedia.org/wiki/Body_mass_index#Categories
-        if bodymassindex < 15.0:
-            bot.reply("Your BMI is %s, very severely underweight. You have to gain %skg to become normal weight." % (bodymassindex, to_gain))
-        elif 15.0 <= bodymassindex < 16.0:
-            bot.reply("Your BMI is %s, severely underweight. You have to gain %skg to become normal weight." % (bodymassindex, to_gain))
-        elif 16.0 <= bodymassindex < 18.5:
-            bot.reply("Your BMI is %s, underweight. You have to gain %skg to become normal weight." % (bodymassindex, to_gain))
-        elif 18.5 <= bodymassindex < 25.0:
-            bot.reply("Your BMI is %s, healthy weight." % (bodymassindex))
-        elif 25.0 <= bodymassindex < 30.0:
-            bot.reply("Your BMI is %s, overweight. You have to lose %skg to become normal weight." % (bodymassindex, to_lose))
-        elif 30.0 <= bodymassindex < 35.0:
-            bot.reply("Your BMI is %s, moderately obese. You have to lose %skg to become normal weight." % (bodymassindex, to_lose))
-        elif 35.0 <= bodymassindex < 40.0:
-            bot.reply("Your BMI is %s, severely obese. You have to lose %skg to become normal weight." % (bodymassindex, to_lose))
+        #Vertaa painoindeksia asettamiini arvoihin
+        if fatness < 17.0:
+            if round(fatness, 2) == 17.0:
+                bot.reply("painoindeksisi on hitusen alle %s eli astraalipaino 5/5" % (round(fatness, 2)))
+            else:
+                bot.reply("painoindeksisi on %s eli astraalipaino 5/5" % (round(fatness, 2)))
+        elif 17.0 <= fatness < 18.5:
+            if round(fatness) == 18.5:
+                bot.reply("painoindeksisi on hitusen alle %s eli et oo sami (mut laihduta yli %s kiloo tai kasva pituutta %s senttii et oot astraalitier)" % (round(fatness, 2), laihdutettava, kasvettava))
+            else:
+                bot.reply("painoindeksisi on %s eli et oo sami (mut laihduta yli %s kiloo tai kasva pituutta %s senttii et oot astraalitier)" % (round(fatness, 2), laihdutettava, kasvettava))
+        elif 18.5 <= fatness < 20:
+            if round(fatness) == 20:
+                bot.reply("vitun sami :D sul kyl viel toivoo ku bmi hitusen alle 20 (kahen desimaalin tarkkuudel se on %s). laihduta %s kiloo tai kasva %s senttii nii oot astraalitieris" % (round(fatness, 2), laihdutettava, kasvettava))
+            else:
+                bot.reply("vitun sami :D sul kyl viel toivoo ku bmi alle 20 (kahen desimaalin tarkkuudel se on %s). laihduta %s kiloo tai kasva %s senttii nii oot astraalitieris" % (round(fatness, 2), laihdutettava, kasvettava))
         else:
-            bot.reply("Your BMI is %s, very severely obese. You have to lose %skg to become normal weight." % (bodymassindex, to_lose))
+            bot.reply("EI JUMALAUTA EI HELVETTI BMI %s SAATANAN SAMI JUMALAUTA EI VITTU SULLA ON YLI %s KILOO LAIHDUTETTAVANA TAI %s SENTTII KASVETTAVANA ASTRAALITIERII EI HELVETTI OIKEESTI" % (fatness, laihdutettava, kasvettava))
