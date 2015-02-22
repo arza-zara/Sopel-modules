97646e1ce63d0770d1af8ef8c417f578af8ef822
Meicceli <meicceli@github.mail.kapsi.fi>
2015-02-22 15:57:14 +0200

    [kuha.py] Now supports .kuha <number>

--- kuha.py
+++ kuha.py
@@ -8,16 +8,23 @@ License Version 3 (or greater at your wish).
 """
 from urllib import urlopen
 from bs4 import BeautifulSoup
-from willie.module import commands
+from willie.module import commands, example
 
 
 @commands('kuha')
+@example('.kuha 69')
 def lannistaja_kuha(bot, trigger):
-    url = urlopen("http://lannistajakuha.com/random").read()
+    number = ""
+    if trigger.group(2):
+        try:
+            number = str(int(trigger.group(2).split(" ")[0]))
+        except:
+            pass
+    if len(number) > 0:
+        url = urlopen("http://lannistajakuha.com/" + number).read()
+    else:
+        url = urlopen("http://lannistajakuha.com/random").read()
     soup = BeautifulSoup(url)
     result = (soup.find("p", attrs={"class": "teksti"}))
     result = result.text.strip()
-    if trigger.group(3) is None:
-        bot.say(result)
-    else:
-        bot.say("%s: %s" % (trigger.group(2), result))
+    bot.say(result)
