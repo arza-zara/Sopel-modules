a6d31eb4d04c87e1053e78daf3cf6a1c04c05475
Meicceli <meicceli@github.mail.kapsi.fi>
2015-05-24 16:37:51 +0300

    [vimeo.py] Added vimeo.py

---
+++ vimeo.py
@@ -0,0 +1,51 @@
+# coding: utf8
+"""
+vimeo.py - Willie Vimeo Module
+Copyright © 2015, Marcus Leivo
+
+Licensed under the GNU Lesser General Public
+License Version 3 (or greater at your wish).
+"""
+from __future__ import unicode_literals
+import json
+import re
+
+from willie import web
+from willie.module import commands, example, rule
+
+
+regex = re.compile('.*(vimeo.com/)([0-9]+)')
+
+
+def setup(bot):
+    if not bot.memory.contains('url_callbacks'):
+        bot.memory['url_callbacks'] = tools.WillieMemory()
+    bot.memory['url_callbacks'][regex] = vimeo_by_url
+
+
+@rule('.*(vimeo.com/)([0-9]+)')
+def vimeo_by_url(bot, trigger, found_match=None):
+    match = found_match or trigger
+    videoID = match.group(2)
+    apiURL = "https://vimeo.com/api/v2/video/" + videoID + ".json"
+    try:
+        resp = json.loads(web.get(apiURL))
+    except:
+        return
+
+    output = u"[Vimeo] "
+    output += u"Title: %s" % (unicode(resp[0]['title']))
+    if 'user_name' in resp[0]:
+        output += u" | Uploader: %s" % (unicode(resp[0]['user_name']))
+    if 'upload_date' in resp[0]:
+        output += u" | Uploaded: %s" % (unicode(resp[0]['upload_date']))
+    if 'duration' in resp[0]:
+        output += u" | Duration: %s" % (unicode(resp[0]['duration']))
+    if 'stats_number_of_plays' in resp[0]:
+        output += u" | Views : %s" % (unicode(resp[0]['stats_number_of_plays']))
+    if 'stats_number_of_comments' in resp[0]:
+        output += u" | Comments: %s" % (unicode(resp[0]['stats_number_of_comments']))
+    if 'stats_number_of_likes' in resp[0]:
+        output += u" | Likes: %s" % (unicode(resp[0]['stats_number_of_likes']))
+
+    bot.say(output)
